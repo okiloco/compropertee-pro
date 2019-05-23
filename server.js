ebedc3583cf1d8de66f5359e453fb0d0a3bc9a7c
Fabian Vargas <okiloco2@gmail.com>
2019-05-23 18:13:25 -0500

    server-compression

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+var compression = require('compression')
 const express = require('express');
 const path = require('path');
 var secure = require('express-force-https')
 const port = process.env.PORT || 8080;
 const app = express();
-
+app.use(compression())
 // serve static assets normally
 app.use(secure);
 app.use(express.static(__dirname + '/build'));
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'build/index.html'));
+response.sendFile(path.resolve(__dirname, 'build/index.html'));
 });
 
 app.listen(port);
