0ece14305313bdeb176db44d7f754b6f8c5e118a
Fabian Vargas <okiloco2@gmail.com>
2019-05-28 20:52:38 -0500

    Server Cache COntroller

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 var compression = require('compression')
+var CacheControl = require("express-cache-controller");
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 var secure = require('express-force-https')
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(compression())
 // serve static assets normally
-app.use(secure);
+//app.use(secure);
 app.use(express.static(__dirname + '/build'));
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
+
+app.use(cacheControl({ maxAge: 60 }));
 app.get('*', function (request, response) {
 response.sendFile(path.resolve(__dirname, 'build/index.html'));
 });
 
-app.listen(port);
+https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/compropertee.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/compropertee.com/fullchain.pem')
+}, app)
+.listen(port);
 console.log("Compropertee Dashboard Started on port " + port);
