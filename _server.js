361abb19620032a3663459fc75635dfdc9d61ce9
Fabian Vargas <okiloco2@gmail.com>
2019-06-19 12:25:09 -0500

    Server js

---
+++ _server.js
@@ -0,0 +1,27 @@
+var compression = require('compression')
+var cacheControl = require("express-cache-controller");
+const express = require('express');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+var secure = require('express-force-https')
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(compression())
+// serve static assets normally
+//app.use(secure);
+app.use(express.static(__dirname + '/build'));
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+
+app.use(cacheControl({ maxAge: 1080 }));
+app.get('*', function (request, response) {
+response.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
+
+https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/compropertee.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/compropertee.com/fullchain.pem')
+}, app)
+.listen(port);
+console.log("Compropertee Dashboard Started on port " + port);
